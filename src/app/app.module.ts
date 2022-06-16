555a6ecd156fc75f0d513e91a2c12c535febdcae
Víctor Martínez <ahal_tocob@hotmail.com>
2022-06-15 20:18:18 -0500

    AdminLte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './templates/navbar/navbar.component';
+import { SidebarMenuComponent } from './templates/sidebar-menu/sidebar-menu.component';
+import { FooterComponent } from './templates/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
